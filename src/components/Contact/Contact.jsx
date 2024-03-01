8fd58771f6e88ba7a66e32144bc078106146b923
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-03-01 20:09:55 +0100

    links added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,16 +15,16 @@ function Contact() {
 
             <div className="buttons-contact">
                 <div>
-                    <button className="btn btn-whatsapp full-w">WhatsApp</button>
-                    <button className="btn btn-twitter full-w">Twitter</button>
+                    <a href="https://wa.me/+33 6 46 07 46 16?text=Hey%20Maxime" target='_blank' rel='noreferrer' className="btn btn-whatsapp full-w">WhatsApp</a>
+                    <a href="https://twitter.com/Izuki78" target='_blank' rel='noreferrer' className="btn btn-twitter full-w">Twitter</a>
                 </div>
                 <div>
-                    <button className="btn btn-instagram full-w">Instagram</button>
-                    <button className="btn btn-primary full-w">Mail</button>
+                    <a href="https://www.instagram.com/maxime.designer_/" target='_blank' rel='noreferrer' className="btn btn-instagram full-w">Instagram</a>
+                    <a href="#" className="btn btn-primary full-w" target='_blank' rel='noreferrer'>Mail</a>
                 </div>
                 <div>
-                    <button className="btn btn-primary full-w">Télécharger mon CV</button>
-                    <button className="btn btn-primary full-w">BOOK</button>
+                    <a href="#" className="btn btn-primary full-w" target='_blank' rel='noreferrer'>Télécharger mon CV</a>
+                    <a href="#" className="btn btn-primary full-w" target='_blank' rel='noreferrer'>BOOK</a>
                 </div>
             </div>
         </div>
